a049b500acab14bf536b5d15bf041a1be32e749f
WilliamLam94 <william.tup.lam@gmail.com>
2023-09-21 23:26:54 +0200

    javascript comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+// Getting elements from DOM
+const hamburgerEl = document.querySelector('.hamburger');
+const navMenuEl = document.querySelector('.nav-menu');
+const navLinksEl = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	navMenu.classList.toggle('active');
+// Adding a click event for the hamburger and navMenu elements by toggling classnames.
+hamburgerEl.addEventListener('click', () => {
+	hamburgerEl.classList.toggle('active');
+	navMenuEl.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-	n.addEventListener('click', () => {
-		hamburger.classList.remove('active');
-		navMenu.classList.remove('active');
+// When clicking on link in nav we remove the active classname.
+// But only when the navMenuEl is visible.
+navLinksEl.forEach((link) =>
+	link.addEventListener('click', () => {
+		hamburgerEl.classList.remove('active');
+		navMenuEl.classList.remove('active');
 	})
 );
